d5bc1a35421e528d768ed55eff85536f5654bb6d
Suleman <mohdsulemanhussain@gmail.com>
2018-07-07 23:33:45 +0530

    Initial commit

---
+++ scripts/custom.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+    var selectedvalue="";
+     
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      slidesPerGroup: 4,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+    $('.carousel').carousel({
+      interval: 2000,
+    });
+
+    // $('.carousel-control-prev').carousel('prev');
+    // $('.carousel-control-next').carousel('next');
+
+    var availableTags = [
+      "Asp.net MVC",
+      "C",
+      "C++",
+      "COBOL",
+      "Java",
+      "JavaScript",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#txtSearch" ).autocomplete({
+      source: availableTags,
+      autofill: true,
+      select: function(e, ui){
+        selectedvalue = ui.item.label;
+        $('.modal').modal('show');
+        $('#lblselectedValue').text(selectedvalue);
+      }
+    });
+
+    $('#btnSearch').on('click', function(){
+      alert("you have searched for "+selectedvalue);
+      $('.modal').modal('show');
+      $('#lblselectedValue').text(selectedvalue);
+    });
+
+    $('.rowCard').on('click', function(){
+      location.href = "details.html";
+    })
+
+    var bgimg = ["images/img1.jpeg", "images/img2.jpg", "images/img3.jpg"];
+    var len = bgimg.length;
+    var imgCnt = 0;
+    doScroll(imgCnt);
+    setInterval(function(){
+      doScroll(imgCnt);
+      imgCnt++;
+      if(imgCnt >= len) { imgCnt =0;}
+    }, 2000);
+
+
+    function doScroll(i) {
+      console.log(bgimg[i]);
+      $('.bgImg').css("background-image", "url("+ bgimg[i]+")");
+    }
+
+  });
+        
